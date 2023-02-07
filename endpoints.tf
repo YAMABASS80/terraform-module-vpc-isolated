ab65a0865c16d39ae9172e737f7a69877a0dcd97
Yamabe <myamabe@88665a14de73.ant.amazon.com>
2023-02-07 13:09:16 +0900

    Add S3 endpoint

--- endpoints.tf
+++ endpoints.tf
@@ -13,3 +13,22 @@ resource "aws_vpc_endpoint" "endpoints" {
     aws_subnet.private_subnet_2.id,
   ]
 }
+
+## VPC Endpoint for Amazon S3 (Gateway)
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = aws_vpc.this.id
+  service_name = "com.amazonaws.${data.aws_region.current.name}.s3"
+  tags = {
+    "Name" = "${var.resource_tag_prefix}_s3_endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint_route_table_association" "private_subnet_1" {
+  route_table_id  = aws_route_table.private_subnet_1_route_table.id
+  vpc_endpoint_id = aws_vpc_endpoint.s3.id
+}
+
+resource "aws_vpc_endpoint_route_table_association" "private_subnet_2" {
+  route_table_id  = aws_route_table.private_subnet_2_route_table.id
+  vpc_endpoint_id = aws_vpc_endpoint.s3.id
+}
