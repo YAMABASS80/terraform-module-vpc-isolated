91b344c84f54cb5dcd7dddf66c3e01e9dad5a473
Yamabe <myamabe@88665a14de73.ant.amazon.com>
2023-02-07 13:07:53 +0900

    revamp the way of creating vpc endpoints (interface). Re implemented with for_each loop

--- endpoints.tf
+++ endpoints.tf
@@ -1,6 +1,10 @@
-resource "aws_vpc_endpoint" "ssm" {
+resource "aws_vpc_endpoint" "endpoints" {
   vpc_id            = aws_vpc.this.id
-  service_name      = "com.amazonaws.${data.aws_region.current.name}.ssm"
+  for_each = toset(var.vpc_endpoints) 
+  service_name      = "com.amazonaws.${data.aws_region.current.name}.${each.key}"
+  tags = {
+    "Name" = "${var.resource_tag_prefix}_${each.key}_endpoint"
+  }
   vpc_endpoint_type = "Interface"
   security_group_ids = [aws_security_group.endpoint_sg.id]
   private_dns_enabled = true
@@ -8,37 +12,4 @@ resource "aws_vpc_endpoint" "ssm" {
     aws_subnet.private_subnet_1.id,
     aws_subnet.private_subnet_2.id,
   ]
-  tags = {
-    "Name" = "${var.resource_tag_prefix}_ssm"
-  }
-}
-
-resource "aws_vpc_endpoint" "ec2messages" {
-  vpc_id            = aws_vpc.this.id
-  service_name      = "com.amazonaws.${data.aws_region.current.name}.ec2messages"
-  vpc_endpoint_type = "Interface"
-  security_group_ids = [aws_security_group.endpoint_sg.id]
-  private_dns_enabled = true
-  subnet_ids = [
-    aws_subnet.private_subnet_1.id,
-    aws_subnet.private_subnet_2.id,
-  ]
-  tags = {
-    "Name" = "${var.resource_tag_prefix}_ec2messages"
-  }
-}
-
-resource "aws_vpc_endpoint" "ssmmessages" {
-  vpc_id            = aws_vpc.this.id
-  service_name      = "com.amazonaws.${data.aws_region.current.name}.ssmmessages"
-  vpc_endpoint_type = "Interface"
-  security_group_ids = [aws_security_group.endpoint_sg.id]
-  private_dns_enabled = true
-  subnet_ids = [
-    aws_subnet.private_subnet_1.id,
-    aws_subnet.private_subnet_2.id,
-  ]
-  tags = {
-    "Name" = "${var.resource_tag_prefix}_ssmmessages"
-  }
 }
